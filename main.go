d11378e185bdba6aa6af607e22addfc563b419e6
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-01 20:01:11 -0500

    feat(root): Remove basic handlers logic

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,48 +15,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-
-	fmt.Fprintf(w, "This is the about page and 2 + 2 is %d", sum)
-}
-
-// addValues adds two integers and return the sum
-func addValues(x, y int) int {
-	return x + y
-}
-
-// divideValues divide two float64 and return the division
-func divideValues(x, y float64) (float64, error) {
-	if y == 0 {
-		err := errors.New("cannot divide by zero")
-
-		return 0, err
-	}
-
-	result := x / y
-
-	return result, nil
-}
-
-// Divide is the divide page handler
-func Divide(w http.ResponseWriter, r *http.Request) {
-	x, y := 100.0, 0.0
-
-	f, err := divideValues(x, y)
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-
-		return
-	}
-
-	fmt.Fprintf(w, "%f divided by %f is %f", x, y, f)
 }
 
 // main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	log.Printf("Starting application on port %s\n", portNumber)
 
