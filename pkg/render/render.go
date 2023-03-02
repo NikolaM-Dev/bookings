7d47de6af09633f30d20c2cbe9e780c4c9d389bf
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-01 20:49:40 -0500

    docs(render): Add RenderTemplate docs

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
 	err := parsedTemplate.Execute(w, nil)
