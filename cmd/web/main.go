4fea32ceee17d9c4d85937489ea5ee75b043db47
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-02 19:11:10 -0500

    feat(main): Add template cache to global config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,13 +4,24 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/NikolaM-Dev/bookings/pkg/config"
 	"github.com/NikolaM-Dev/bookings/pkg/handlers"
+	"github.com/NikolaM-Dev/bookings/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
