846cfa987fabba5d8eede4854a4560ca95b935cd
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-06 14:37:43 -0500

    feat(handlers): Add repository pattern

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,15 +3,36 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/NikolaM-Dev/bookings/pkg/config"
 	"github.com/NikolaM-Dev/bookings/pkg/render"
 )
 
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home")
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about")
 }
