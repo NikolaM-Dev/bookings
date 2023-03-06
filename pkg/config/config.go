8eba888d540f35a5a80df2e4d39db99d10950448
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-06 14:43:25 -0500

    feat(config): Remove unused attribute

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,12 +2,10 @@ package config
 
 import (
 	"html/template"
-	"log"
 )
 
 // AppConfig holds the application config
 type AppConfig struct {
-	InfoLog       *log.Logger
 	TemplateCache map[string]*template.Template
 	UseCache      bool
 }
